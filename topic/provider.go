d3b208540824746dcaf2dc9efcca201c38e4ada0
Matt Jones <matthew.jones@deliveroo.co.uk>
2020-07-06 08:22:36 +0100

    fix function signature with correct type
    in tf 0.12+ trying to return []string instead of []interface{} causes the following error:
    
    `Error: rpc error: code = Canceled desc = context canceled`

--- topic/provider.go
+++ topic/provider.go
@@ -3,7 +3,6 @@ package topic
 import (
 	"log"
 	"os"
-	"strings"
 	"sync"
 
 	"github.com/Shopify/sarama"
@@ -78,12 +77,13 @@ func configure(d *schema.ResourceData) (interface{}, error) {
 		cfg.Net.SASL.Password = v.(string)
 	}
 
-	var hosts []string
-	for _, host := range d.Get("hosts").([]interface{}) {
-		hosts = append(hosts, host.(string))
+	hostsConfig := d.Get("hosts").([]interface{})
+	if hostsConfig == nil || len(hostsConfig) < 1 {
+		hostsConfig, _ = getHosts()
 	}
-	if hosts == nil {
-		hosts, _ = getHosts()
+	var hosts []string
+	for _, host := range hostsConfig {
+		hosts = append(hosts, host.(string))
 	}
 
 	log.Printf("[INFO] Initializing Kafka client with hosts: %v\n", hosts)
@@ -96,11 +96,15 @@ func configure(d *schema.ResourceData) (interface{}, error) {
 	return &threadsafeClient{client, new(sync.Mutex)}, nil
 }
 
-func getHosts() ([]string, error) {
+func getHosts() ([]interface{}, error) {
 	hosts := os.Getenv("KAFKA_HOSTS")
 	log.Printf("[INFO] hosts: %v\n", hosts)
 	if hosts == "" {
-		return []string{}, nil
+		return []interface{}{}, nil
 	}
-	return strings.Split(hosts, ","), nil
+	res := make([]interface{}, len(hosts))
+	for i, v := range hosts {
+		res[i] = v
+	}
+	return res, nil
 }
